8ae03c55c306ffdbbb042655f29bddbac946f236
Andrew Musselman <andrew.mussels@gmail.com>
2022-06-01 20:47:12 -0600

    Add useEffect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Articles from "./Articles/Articles";
+import getData from "./apiCalls";
+import { useState, useEffect } from 'react';
 
-function App() {
+
+const App = () => {
+  let [articles, setArticles] = useState([]);
+
+  const getArticles = async () => {
+    let retrievedArticles = await getData()
+    setArticles(retrievedArticles)
+  }
+
+  useEffect(() => {
+    getArticles()
+  }, [])
+
   return (
-    <>
-    <Header />
-    <Articles />
-    <Footer />
-    </>
+    <div>
+      <h2>{process.env.REACT_APP_NYT_API_KEY}</h2>
+      <Header />
+      <Articles />
+      <Footer />
+    </div>
   );
 }
 
