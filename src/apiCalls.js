709c1e59449b260f6ee01f4ad691f308209d03c2
Andrew Musselman <andrew.mussels@gmail.com>
2022-06-02 10:50:18 -0600

    Remove console log

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,18 @@
 const key = process.env.REACT_APP_NYT_API_KEY
-console.log(key)
 const path = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=home&api-key=AU000jHu07nh6YDudByMHIPWS1kq7jJ1`
 
 
 const getData = async () => {
     let response = await fetch(path)
-    await checkResponse(response)
+    let checkedResponse = await checkResponse(response)
+    return checkedResponse
 }
 
 const checkResponse = (res) => {
-    if(res.ok) {
-        return res.json()
-    } else {
+    if(!res.ok) {
         throw new Error('Something went wrong')
+    } else {
+        return res.json()
     }
 }
 
