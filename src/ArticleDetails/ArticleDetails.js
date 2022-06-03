5a6b804b0565c8b28d9e038b4c36871551d15f52
Andrew Musselman <andrew.mussels@gmail.com>
2022-06-02 23:16:15 -0600

    Add details to the details component

--- src/ArticleDetails/ArticleDetails.js
+++ src/ArticleDetails/ArticleDetails.js
@@ -1,7 +1,18 @@
 const ArticleDetails = ({details}) => {
     return (
         <article>
-            <p>Hello world</p>
+            <h2>{details.headline.main}</h2>
+            <h3>{details.byline.original}</h3>
+            <p>Section: {details.section_name}</p>
+            <p>{details.snippet}</p>
+            <p>{details.lead_paragraph}</p>
+            <a href={details.web_url}>Read Here</a>
+            <ul>
+                {details.keywords.map((word, i)=> {
+                    return <li key={i}>{word.value}</li>
+                })}
+            </ul>
+
         </article>
     )
 }
