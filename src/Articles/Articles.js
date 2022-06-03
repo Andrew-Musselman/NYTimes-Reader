3b16766f328c954765de0bf9b9033045a845a0d3
Andrew Musselman <andrew.mussels@gmail.com>
2022-06-02 21:31:27 -0600

    Add dynamic links to each article componnet

--- src/Articles/Articles.js
+++ src/Articles/Articles.js
@@ -1,14 +1,18 @@
-import Article from "../Article/Article"
+import Article from "../Article/Article";
+import { Link } from "react-router-dom";
 
 const Articles = ({articles}) => {
     let articlesMapped = articles.map((article, i) => {
        return ( 
-        <Article 
-                key={i}
-                headline={article.headline.main}
-                author={article.byline.original}
-                synopsis={article.snippet}
-            />
+        <Link to={`/${i}`}>
+            <Article 
+                    key={i}
+                    headline={article.headline.main}
+                    author={article.byline.original}
+                    synopsis={article.snippet}
+                />
+        </Link>
+
     )})
 
     return (
