5554de2021c535fb8f52c1066da661d6eb0681ab
Andrew Musselman <andrew.mussels@gmail.com>
2022-06-03 09:08:05 -0600

    Render filter component in header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.css';
+import Filter from '../Filter/Filter';
 
-const Header = () => {
+const Header = ({getSearchResults}) => {
     return (
         <nav>
             <h1>NYTimes README</h1>
+            <Filter getSearchResults={getSearchResults}/>
         </nav>
     )
 }
